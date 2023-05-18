07192d59dce4ed4e45033b58a76a8d5a75360320
Jake <denley420@gmail.com>
2023-05-18 09:29:25 +0800

    edit resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -114,7 +114,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1q71FP-OFU23NYA_Ur3aCaVRxmizzdgRJ/view?usp=sharing"
+              href="https://drive.google.com/file/d/1829BcIdBhm4Opc7SS_iVzzUesL8-tYaL/view?usp=share_link"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
